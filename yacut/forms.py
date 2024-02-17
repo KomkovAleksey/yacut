410e4fc2622d0293508d8609c2d4e93d7d465d7b
Алексей Комков <ale45883412@yandex.ru>
2024-02-17 18:13:31 +0700

    make URLForm

---
+++ yacut/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, URLField, StringField
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
+
+
+class URLForm(FlaskForm):
+    original_link = URLField(
+        'Длинная ссылка.',
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            Length(1, 256),
+            URL(require_tld=True, message='Проверьте вводимый адрес ссылки.')
+        ]
+    )
+    custom_id = StringField(
+        'Ваш вариант короткой ссылки.',
+        validators=[
+            Length(1, 16, message='Длинна ссылки болше 16 символов.'),
+            Optional(),
+            Regexp(
+                regex=r'^[A-Za-z0-9]+$',
+                message='Недопустимое имя короткой ссылки.'
+            ),
+        ]
+    )
+    create = SubmitField('Создать')
