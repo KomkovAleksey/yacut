c7a53d5d44270b488931d9fa9048010610822d47
Алексей Комков <ale45883412@yandex.ru>
2024-02-18 02:20:16 +0700

    test_config.py:12: AssertionError

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
