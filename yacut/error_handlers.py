086d7f740c222e714bedb6bd5d03d80dbbbac279
Алексей Комков <ale45883412@yandex.ru>
2024-02-17 21:50:15 +0700

    HTTPStatus

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -28,12 +28,12 @@ def invalid_api_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 
-@app.errorhandler(404)
+@app.errorhandler(HTTPStatus.NOT_FOUND)
 def page_not_found(error):
     return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
-@app.errorhandler(500)
+@app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
