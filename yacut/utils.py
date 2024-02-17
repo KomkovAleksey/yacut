c7a1431b24d97e83dbb9ad540cd145cd2fbbafea
Алексей Комков <ale45883412@yandex.ru>
2024-02-17 18:40:13 +0700

    get_unique_sh

---
+++ yacut/utils.py
@@ -0,0 +1,33 @@
+import random
+import hashlib
+import re
+
+from flask import flash, render_template, url_for
+
+from . import db
+from .models import URLMap
+from .forms import URLForm
+
+
+def get_unique_short_url(long_url):
+    """Функция формирования короткой ссылки."""
+    short_url = random.choices(
+        hashlib.md5(long_url.encode()).hexdigest(),
+        k=6
+    )
+    return ''.join(short_url)
+
+
+def get_add_url_to_db(short_url):
+    """
+    Функция добавляющая оригинальную и укороченную ссылку в базу данных.
+    Выводит сообщение с укороченной ссылкой на главную страницу.
+    """
+    url_map = URLMap(
+        original=URLForm().original_link.data,
+        short=short_url,
+    )
+    db.session.add(url_map)
+    db.session.commit()
+    flash(url_for('redirect_short_url', short=url_map.short, _external=True), 'short_link')
+    return render_template('yacut.html', form=URLForm())
