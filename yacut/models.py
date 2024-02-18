3e46e99fa7c3212505e7a66fa154e3f11c4277eb
Алексей Комков <ale45883412@yandex.ru>
2024-02-18 15:17:27 +0700

    ORIGINAL_MAX_LENGTH, CUSTOM_ID_MAX_LENGTH

--- yacut/models.py
+++ yacut/models.py
@@ -5,15 +5,15 @@ from datetime import datetime
 
 from yacut import db
 
-from .constants import LOCALHOST
+from .constants import LOCALHOST, ORIGINAL_MAX_LENGTH, CUSTOM_ID_MAX_LENGTH
 
 
 class URLMap(db.Model):
     """Модель для работы с ссылками."""
 
     id = db.Column(db.Integer, primary_key=True)
-    original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), unique=True)
+    original = db.Column(db.String(ORIGINAL_MAX_LENGTH), nullable=False)
+    short = db.Column(db.String(CUSTOM_ID_MAX_LENGTH), unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def to_dict(self):
